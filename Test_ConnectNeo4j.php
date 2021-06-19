11ea426d115492d260054a144dce1b6bd179fce7
NgMinhTri <38482726+NgMinhTri@users.noreply.github.com>
2021-06-19 16:06:00 +0700

    test kết nối Neo4j

--- Test_ConnectNeo4j.php
+++ Test_ConnectNeo4j.php
@@ -3,10 +3,11 @@
 require_once 'vendor/autoload.php';
 $client = Laudis\Neo4j\ClientBuilder::create()
     
-    ->addBoltConnection('covid19', 'bolt://neo4j:password@localhost')
-    ->setDefaultConnection('covid19')
+    ->addBoltConnection('neo4j', 'bolt://neo4j:123456789@localhost')
+    ->setDefaultConnection('neo4j')
     ->build();
 
-
+    $results = $client->run('MATCH (n) RETURN n LIMIT 25');
+    print_r($results);
 
  ?>
